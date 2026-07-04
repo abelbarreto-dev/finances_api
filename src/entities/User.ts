986a639f72617e8a71284d13fb864f98ba87e296
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-07-03 23:54:49 -0300

    feat: entity user

---
+++ src/entities/User.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid") id: string;
+
+  @Column({ name: "full_name", type: "varchar", length: 64, nullable: false })
+  fullName: string;
+
+  @Column({ type: "varchar", length: 255, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: "varchar", length: 64, nullable: false, unique: true })
+  username: string;
+
+  @Column({ type: "varchar", length: 255, nullable: false })
+  password: string;
+
+  @Column({ type: "varchar", length: 20, nullable: true })
+  phone?: string;
+
+  @Column({ name: "created_at", type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  createAt: Date;
+
+  @Column({ name: "updated_at", type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+  updatedAt: Date;
+
+  @Column({ name: "deleted_at", type: "timestamp", nullable: true })
+  deletedAt?: Date;
+}
